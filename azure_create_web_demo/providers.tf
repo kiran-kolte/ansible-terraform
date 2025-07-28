aa5ef05b6a97c9b2c0d543aa24e65aeb2dd7eb49
kiran-kolte <kiran.kolte@maersk.com>
2025-07-28 12:22:40 +0530

    Update providers.tf

--- azure_create_web_demo/providers.tf
+++ azure_create_web_demo/providers.tf
@@ -11,7 +11,7 @@ terraform {
     access_key           = ""  # Can also be set via `ARM_ACCESS_KEY` environment variable.
     storage_account_name = ""                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = ""                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    key                  = ""                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
 
