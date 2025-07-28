28ac01b614ac7c08a7f9bfa324ac2fd821bd81d4
kiran-kolte <kiran.kolte@maersk.com>
2025-07-28 11:24:13 +0530

    Update providers.tf

--- azure_create_web_demo/providers.tf
+++ azure_create_web_demo/providers.tf
@@ -7,7 +7,12 @@ terraform {
     }
   }
 
-  backend "azurerm" {}
+  backend "azurerm" {
+    access_key           = ""  # Can also be set via `ARM_ACCESS_KEY` environment variable.
+    storage_account_name = ""                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = ""                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
 }
 
 # Configure the Microsoft Azure Provider
